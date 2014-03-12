7d254999dc794697ec91a4e7095905ae81e165dd
Yorick Schellevis <yorick_sk8@live.nl>
2014-03-12 14:55:17 +0100

    Started on clickable elements for the game screen

---
+++ proef-proeven/Components/Game/ClickAbleObject.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace proef_proeven.Components.Game
+{
+    class ClickAbleObject
+    {
+        private Rectangle hitbox;
+        /// <summary>
+        /// The Hitbox of the object. 
+        /// TODO: Test if this alters the private hitbox. It shouldn't do that
+        /// </summary>
+        public Rectangle Hitbox
+        {
+            get 
+            { 
+                return hitbox; 
+            }
+
+            protected set 
+            { 
+                hitbox = value; 
+            }
+        }
+
+        private Texture2D image;
+        /// <summary>
+        /// The image of the clickable object
+        /// </summary>
+        public Texture2D Image
+        {
+            get
+            {
+                return image;
+            }
+
+            set
+            {
+                image = value;
+                hitbox.Width = value.Width;
+                hitbox.Height = value.Height;
+            }
+        }
+
+        private Vector2 position;
+        /// <summary>
+        /// The position of the clickable object
+        /// </summary>
+        public Vector2 Position
+        {
+            get { return position; }
+
+            set
+            {
+                position = value;
+
+                hitbox.X = (int)value.X;
+                hitbox.Y = (int)value.Y;
+            }
+        }
+
+        public ClickAbleObject()
+        {
+            hitbox = new Rectangle();
+        }
+
+        public virtual void Update(GameTime dt)
+        {
+
+        }
+
+        public virtual void Draw(SpriteBatch batch)
+        {
+
+        }
+    }
+}
